d5555cc9f86eba84896790a58142d0b250fedf80
Daniil <Daniil.Yakovenko@artezio.com>
2019-02-14 16:15:27 +0300

    fix: react + webpack init
    tsconfig.json added
    created test component BaseLayout
    resolution changed to tsx
    
    task ARTFORMS-3

---
+++ packages/designer/src/components/BaseLayout.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+export class BaseLayout extends React.Component {
+    render() {
+        return (
+            <div>
+                {'Hello!'}
+            </div>
+        )
+    }
+}
+
+export default BaseLayout;
---
+++ packages/designer/src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from "react-dom";
+import BaseLayout from './components/BaseLayout';
+
+
+ReactDOM.render(<BaseLayout />, document.getElementById('root'));
