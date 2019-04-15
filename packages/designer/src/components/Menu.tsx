24b4e78df6715de34c2870acf00270b16955937d
Daniil <Daniil.Yakovenko@artezio.com>
2019-04-15 14:38:19 +0300

    feat: created Menu component instead of dropdown menu
    deleted dropdown menu;

--- packages/designer/src/components/Menu.tsx
+++ packages/designer/src/components/Menu.tsx
@@ -1,24 +1,10 @@
 import * as React from 'react';
-import { MenuProps, MenuItem } from '../interfaces/components/MenuProps';
+import { MenuProps } from '../interfaces/components/MenuProps';
 import * as Models from '@art-forms/models';
 
 
 export class Menu extends React.Component<MenuProps> {
     itemFactory: Models.ItemFactory = new Models.ItemFactory(this.props.item);
-    items: MenuItem[] = [
-        { title: 'Add display text', action: this.addItem.bind(this) },
-        { title: 'Add group', action: this.addGroupItem.bind(this) },
-        { title: 'Add text question', action: this.addTextItem.bind(this) },
-        { title: 'Add string question', action: this.addStringItem.bind(this) },
-        { title: 'Add decimal question', action: this.addDecimalItem.bind(this) },
-        { title: 'Add boolean question', action: this.addBooleanItem.bind(this) },
-        { title: 'Add time question', action: this.addTimeItem.bind(this) },
-        { title: 'Add date question', action: this.addDateItem.bind(this) },
-        { title: 'Add date-time question', action: this.addDateTimeItem.bind(this) },
-        { title: 'Add attachment question', action: this.addAttachmentItem.bind(this) },
-        { title: 'Add choice question', action: this.addChoiceItem.bind(this) },
-        { title: 'Add open-choice question', action: this.addOpenChoiceItem.bind(this) }
-    ]
 
     addItem() {
         const { item } = this.props;
@@ -31,76 +17,17 @@ export class Menu extends React.Component<MenuProps> {
         const newItem = this.itemFactory.createGroupItem();
         item.addItem(newItem);
     }
-
-    addTextItem() {
-        const { item } = this.props;
-        const newItem = this.itemFactory.createTextItem();
-        item.addItem(newItem);
-    }
-
-    addStringItem() {
-        const { item } = this.props;
-        const newItem = this.itemFactory.createStringItem();
-        item.addItem(newItem);
-    }
-
-    addDecimalItem() {
-        const { item } = this.props;
-        const newItem = this.itemFactory.createDecimalItem();
-        item.addItem(newItem);
-    }
-
-    addBooleanItem() {
-        const { item } = this.props;
-        const newItem = this.itemFactory.createBooleanItem();
-        item.addItem(newItem);
-    }
-
-    addTimeItem() {
-        const { item } = this.props;
-        const newItem = this.itemFactory.createTimeItem();
-        item.addItem(newItem);
-    }
-
-    addDateItem() {
-        const { item } = this.props;
-        const newItem = this.itemFactory.createDateItem();
-        item.addItem(newItem);
-    }
-
-    addDateTimeItem() {
-        const { item } = this.props;
-        const newItem = this.itemFactory.createDateTimeItem();
-        item.addItem(newItem);
-    }
-
-    addAttachmentItem() {
-        const { item } = this.props;
-        const newItem = this.itemFactory.createAttachmentItem();
-        item.addItem(newItem);
-    }
-
     addChoiceItem() {
         const { item } = this.props;
         const newItem = this.itemFactory.createChoiceItem({ options: [Models.ChoiceOptionFactory.createChoiceOption()] });
         item.addItem(newItem);
     }
 
-    addOpenChoiceItem() {
-        const { item } = this.props;
-        const newItem = this.itemFactory.createOpenChoiceItem({ options: [Models.ChoiceOptionFactory.createChoiceOption()] });
-        item.addItem(newItem);
-    }
-
     render() {
-        const { title } = this.props;
-        return <div>
-            <button className="btn btn-sm dropdown-toggle" role="button" id="context-menu-link" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                {title}
-            </button>
-            <div className="dropdown-menu" aria-labelledby="context-menu-link">
-                {this.items.map((item, i) => <button key={i} className="dropdown-item btn" onClick={item.action}>{item.title}</button>)}
-            </div>
+        return <div className="menu btn-group">
+            <a className="btn btn-outline-secondary" title="Add text" href="javascript:void(0)" onClick={this.addItem.bind(this)}><i className="fas fa-align-left"></i></a>
+            <a className="btn btn-outline-secondary" title="Add group" href="javascript:void(0)" onClick={this.addGroupItem.bind(this)}><i className="fas fa-layer-group"></i></a>
+            <a className="btn btn-outline-secondary" title="Add question" href="javascript:void(0)" onClick={this.addChoiceItem.bind(this)}><i className="fas fa-plus"></i></a>
         </div>
     }
 }
--- packages/designer/src/interfaces/components/MenuProps.ts
+++ packages/designer/src/interfaces/components/MenuProps.ts
@@ -2,13 +2,8 @@ import * as Models from '@art-forms/models';
 
 
 export interface MenuProps {
-    item: Models.GroupItem;
-    title: string;
-}
-
-export interface MenuItem {
-    title: string;
-    action: any;
+    item: Models.GroupItem | Models.Questionnaire;
+    title?: string;
 }
 
 export default MenuProps;
