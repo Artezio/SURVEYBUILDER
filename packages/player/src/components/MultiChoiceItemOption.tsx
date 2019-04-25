e468af1ec96ca183ce64966ea3f629b4b481824f
Daniil <Daniil.Yakovenko@artezio.com>
2019-04-25 10:08:28 +0300

    feat: multiChoiceItem completed in player

--- packages/player/src/components/MultiChoiceItemOption.tsx
+++ packages/player/src/components/MultiChoiceItemOption.tsx
@@ -6,6 +6,7 @@ import { Checkbox, FormApi, Form } from 'informed';
 export class MultiChoiceItemOption extends React.Component<MultiChoiceItemOptionProps> {
     formApi!: FormApi<Models.IAnswer<any>>;
     answerFactory: Models.AnswerFactory;
+    answer?: Models.Answer<any>;
 
     constructor(props: MultiChoiceItemOptionProps) {
         super(props);
@@ -22,14 +23,21 @@ export class MultiChoiceItemOption extends React.Component<MultiChoiceItemOption
     }
 
     handleSubmit(values: Partial<Models.IAnswer<any>>) {
-        console.log(values);
+        const { option, questionnaireResponseItem } = this.props;
+        if (values.value) {
+            this.answer = this.answerFactory.createAnswer({ value: option.value });
+            questionnaireResponseItem.addAnswer(this.answer);
+        }
+        else {
+            this.answer && this.answer.remove();
+        }
     }
 
     render() {
         const { option } = this.props;
         return <div className="form-check">
             <Form getApi={this.getFormApi.bind(this)} onSubmit={this.handleSubmit.bind(this)}>
-                <Checkbox className="form-check-input" field="value" id={`${option.id}-checkbox`} />
+                <Checkbox className="form-check-input" field="value" id={`${option.id}-checkbox`} onChange={this.submitForm.bind(this)} />
                 <label htmlFor={`${option.id}-checkbox`}>{option.value}</label>
             </Form>
         </div>
