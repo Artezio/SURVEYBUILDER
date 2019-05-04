6fd63644c44775fb618f26bafb2c06855ff81bca
Daniil <Daniil.Yakovenko@artezio.com>
2019-05-04 17:51:23 +0300

    added handeling for control nested lists DnD

--- packages/designer/src/components/QuestionnaireItemList.tsx
+++ packages/designer/src/components/QuestionnaireItemList.tsx
@@ -4,12 +4,38 @@ import ItemWrapper from './ItemWrapper';
 import { Droppable } from 'react-beautiful-dnd';
 
 
-export class QuestionnaireItemList extends React.Component<ItemListProps> {
+export class QuestionnaireItemList extends React.PureComponent<ItemListProps> {
+    state = { isDropDisabled: true };
+
+    onMouseLeave(e: any) {
+        const { container } = this.props;
+        console.log('LEAVE TYPE: ', container.id)
+        !this.state.isDropDisabled && this.setState({ isDropDisabled: true });
+    }
+
+    onMouseOver(e: any) {
+        const { container } = this.props;
+        // console.log('OVER TYPE: ', container.id);
+        if (!e.defaultPrevented) {
+            this.state.isDropDisabled && this.setState({ isDropDisabled: false });
+            e.preventDefault();
+            console.log('OVER_INSIDE TYPE: ', container.id)
+        }
+        else {
+            !this.state.isDropDisabled && this.setState({ isDropDisabled: true });
+        }
+    }
+
     render() {
         const { container, className = '', nestingLevel } = this.props;
-        return <Droppable type={nestingLevel} droppableId={container.id}>
+        return <Droppable type={'nestingLevel'} droppableId={container.id} isDropDisabled={this.state.isDropDisabled}>
             {(provided, snapshot) => (
-                <div className={`questionnaire-item-list${snapshot.isDraggingOver ? ' isDraggingOver' : ''} ${className}`} ref={provided.innerRef} {...provided.droppableProps}>
+                <div {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    onMouseOver={this.onMouseOver.bind(this)}
+                    onMouseLeave={this.onMouseLeave.bind(this)}
+                    className={`questionnaire-item-list${snapshot.isDraggingOver ? ' isDraggingOver' : ''} ${className}`}
+                >
                     {container.items.map((item, i) => <ItemWrapper item={item} key={item.id} nestingLevel={`${nestingLevel}:${i}`} />)}
                     {provided.placeholder}
                 </div>
