ca5a384dc92891b647e659aa1fc696fd7eaea488
Daniil <Daniil.Yakovenko@artezio.com>
2019-05-11 17:59:02 +0300

    fix: disabled autoScroll on mount group

--- packages/designer/src/components/GroupItem.tsx
+++ packages/designer/src/components/GroupItem.tsx
@@ -8,6 +8,7 @@ import QuestionnaireItemList from './QuestionnaireItemList';
 
 export class GroupItem extends React.Component<GroupItemProps> {
     formApi!: FormApi<Partial<Models.IGroupItem>>;
+    inputRef: React.RefObject<HTMLInputElement> = React.createRef();
 
     handleSubmit(values: Partial<Models.IGroupItem>) {
         const { item } = this.props;
@@ -28,6 +29,15 @@ export class GroupItem extends React.Component<GroupItemProps> {
         return <QuestionnaireItemList item={item} nestingLevel={nestingLevel} />
     }
 
+    componentDidMount() {
+        if (this.inputRef.current) {
+            const x = window.pageXOffset;
+            const y = window.pageYOffset;
+            this.inputRef.current.focus();
+            window.scrollTo(x, y)
+        }
+    }
+
     componentDidUpdate() {
         const { item, subscribe } = this.props;
         this.formApi.setValues(item);
@@ -40,7 +50,7 @@ export class GroupItem extends React.Component<GroupItemProps> {
             <Form className="headline" getApi={this.getFormApi.bind(this)} key={item.id} initialValues={item} onSubmit={this.handleSubmit.bind(this)}>
                 <div className="form-group">
                     <label htmlFor={item.id}>Group Title</label>
-                    <Text autoComplete="off" className="form-control" id={item.id} field="text" placeholder="Questions group" autoFocus={true} onBlur={this.submitForm.bind(this)} />
+                    <Text forwardedRef={this.inputRef} autoComplete="off" className="form-control" id={item.id} field="text" placeholder="Questions group" onBlur={this.submitForm.bind(this)} />
                 </div>
             </Form>
             {this.renderItemList()}
