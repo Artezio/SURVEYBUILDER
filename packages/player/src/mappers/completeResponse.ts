5811e5c434e60249453e49d8007685bbe59ea6dc
Daniil <Daniil.Yakovenko@artezio.com>
2019-06-03 15:49:55 +0300

    fix: saved parent while creating new answer

--- packages/player/src/mappers/completeResponse.ts
+++ packages/player/src/mappers/completeResponse.ts
@@ -2,28 +2,21 @@ import * as Models from '@art-forms/models';
 
 export const completeResponse = (item: Models.Questionnaire | Models.GroupItem, response: Models.QuestionnaireResponse | Models.QuestionnaireResponseItem) => {
     item.items && item.items.forEach(item => {
-        let answers;
-        const answerFactory = new Models.AnswerFactory();
+        const responseItem = Models.questionResponseFactory.createResponseByType(item.type, { id: item.id, text: item.text });
+        const answerFactory = new Models.AnswerFactory(responseItem);
         if (item.type !== Models.GROUP) {
-            const responseItem = response.items.find(responseItem => responseItem.id === item.id);
-            if (responseItem && responseItem.answers !== undefined && responseItem.answers.length !== 0) {
-                return;
+            const oldResponseItem = response.items.find(responseItem => responseItem.id === item.id);
+            if (oldResponseItem && Array.isArray(oldResponseItem.answers)) {
+                oldResponseItem.answers.forEach(answer => {
+                    responseItem.addAnswer(answerFactory.createAnswer(answer));
+                })
             }
-            else {
-                answers = Array.isArray((item as Models.QuestionItem<any>).initialAnswers) ?
-                    (item as Models.QuestionItem<any>).initialAnswers.map(initialAnswer => {
-                        if (item.type === Models.CHOICE || item.type === Models.OPEN_CHOICE || item.type === Models.MULTI_CHOICE) {
-                            const option = (item as Models.ChoiceItem).options.find(opt => opt.id === initialAnswer.value);
-                            const value = option && option.value;
-                            const id = option && option.id;
-                            return answerFactory.createAnswer({ id, value });
-                        }
-                        return answerFactory.createAnswer(initialAnswer);
-                    }) :
-                    [];
+            if (Array.isArray((item as Models.QuestionItem<any>).initialAnswers) && responseItem.answers.length === 0) {
+                (item as Models.QuestionItem<any>).initialAnswers.forEach(initialAnswer => {
+                    responseItem.addAnswer(answerFactory.createAnswer(initialAnswer));
+                })
             }
         }
-        const responseItem = Models.questionResponseFactory.createResponseByType(item.type, { id: item.id, text: item.text, answers });
         if (item.type === Models.GROUP) {
             completeResponse(item as Models.GroupItem, responseItem)
         }
