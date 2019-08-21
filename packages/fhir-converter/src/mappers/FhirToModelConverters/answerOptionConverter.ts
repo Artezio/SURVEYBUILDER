92717493ad5bf5b0ac3e8afa60da5ec0a0809c8d
Daniil <Daniil.Yakovenko@artezio.com>
2019-08-21 18:00:46 +0300

    fix: added field initialSelected to answer option mapper

--- packages/fhir-converter/src/mappers/FhirToModelConverters/answerOptionConverter.ts
+++ packages/fhir-converter/src/mappers/FhirToModelConverters/answerOptionConverter.ts
@@ -3,7 +3,8 @@ import * as Models from '@art-forms/models';
 export const mapAnswerOptionToModel = (answerOption: any): Models.IAnswerOption => {
     const newAnswerOption: Models.IAnswerOption = {
         id: answerOption.id,
-        value: answerOption.valueInteger || answerOption.valueDate || answerOption.valueTime || answerOption.valueString
+        value: answerOption.valueInteger || answerOption.valueDate || answerOption.valueTime || answerOption.valueString,
+        defaultSelected: answerOption.initialSelected
     }
     return newAnswerOption;
 }
--- packages/fhir-converter/src/mappers/ModelToFhirCOnverters/answerOptionConverter.ts
+++ packages/fhir-converter/src/mappers/ModelToFhirCOnverters/answerOptionConverter.ts
@@ -3,7 +3,8 @@ import * as Models from '@art-forms/models';
 export const mapAnswerOptionFromModel = (answerOption: Models.IAnswerOption): any => {
     const newAnswerOption: any = {
         id: answerOption.id,
-        valueString: answerOption.value
+        valueString: answerOption.value,
+        initialSelected: answerOption.defaultSelected
     }
     return newAnswerOption;
 }
