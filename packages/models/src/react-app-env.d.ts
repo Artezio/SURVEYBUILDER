e464abffa9eb1363d3ff9e62c2b8c069e1d465fb
Daniil <Daniil.Yakovenko@artezio.com>
2019-08-28 16:31:33 +0300

    fix: added ignore files, removed trash

--- packages/models/src/react-app-env.d.ts
+++ packages/models/src/react-app-env.d.ts
@@ -1,4 +1,3 @@
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
 declare module "json-logic-js" {
-    export const apply = (...any) => boolean;
+    export const apply: (...any) => boolean;
 }
