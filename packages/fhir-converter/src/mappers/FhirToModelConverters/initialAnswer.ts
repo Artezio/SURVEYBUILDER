ce4d9e835eea820c142289c5015571c93d6f3ab1
Daniil <Daniil.Yakovenko@artezio.com>
2019-10-04 15:41:48 +0300

    fix: added valueString to fhir converter

--- packages/fhir-converter/src/mappers/FhirToModelConverters/initialAnswer.ts
+++ packages/fhir-converter/src/mappers/FhirToModelConverters/initialAnswer.ts
@@ -3,7 +3,7 @@ import * as Models from '@art-forms/models';
 export const initialAnswerToModelConverter = (initialAnswer: any): Models.IInitialAnswer<any> => {
     const newInitialAnswer: Models.IInitialAnswer<any> = {
         id: initialAnswer.id,
-        value: initialAnswer.valueBoolean || initialAnswer.valueDecimal || initialAnswer.valueInteger || initialAnswer.valueDate || initialAnswer.valueDateTime || initialAnswer.valueTime
+        value: initialAnswer.valueBoolean || initialAnswer.valueDecimal || initialAnswer.valueInteger || initialAnswer.valueDate || initialAnswer.valueDateTime || initialAnswer.valueTime || initialAnswer.valueString
     }
     return newInitialAnswer;
 }
