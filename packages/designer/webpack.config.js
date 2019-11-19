e8d7ea126f49ba3aed2220791d9eb95b56de12db
Daniil <Daniil.Yakovenko@artezio.com>
2019-11-19 17:35:14 +0300

    feat: built designer library

---
+++ packages/designer/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+    mode: "production",
+    entry: './src/index.ts',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, './dist'),
+        library: 'Designer',
+        libraryTarget: 'umd'
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            }
+        ]
+    },
+    externals: {
+        "react": "react"
+    }
+};
